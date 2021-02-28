bc039ebdce14594c54eccc85c5811fd156e0e01a
Antti Haavikko <antti.haavikko@gmail.com>
2021-02-28 12:39:21 +0200

    Add button click sound

--- Assets/Scripts/CustomButton.cs
+++ Assets/Scripts/CustomButton.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.EventSystems;
+using Random = UnityEngine.Random;
 
 public class CustomButton : Button
 {
@@ -12,6 +13,13 @@ public class CustomButton : Button
     public Image img;
 
     private Color color;
+    private Camera cam;
+
+    protected override void Start()
+    {
+        cam = Camera.main;
+        base.Start();
+    }
 
     public override void OnPointerEnter(PointerEventData eventData)
     {
@@ -22,5 +30,14 @@ public class CustomButton : Button
     public override void OnPointerExit(PointerEventData eventData)
     {
         text.color = img.color = color;
+    }
+
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        var p = cam.ScreenToWorldPoint(transform.position);
+        const float vol = 0.4f;
+        AudioManager.Instance.PlayEffectAt(Random.Range(4, 8), p, 1.1f * vol);
+        AudioManager.Instance.PlayEffectAt(10, p, 0.6f * vol);
+        base.OnPointerClick(eventData);
     }
 }
